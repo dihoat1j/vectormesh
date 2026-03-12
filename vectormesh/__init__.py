282207b8883d7411ad261445dfe39238952ce910
Ivan Bobev <JensonBroski@gmail.com>
2026-03-12 06:13:57 -0700

    Add vectormesh/__init__.py

---
+++ vectormesh/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.1.0"
+
+from vectormesh.engine import MeshEngine
+from vectormesh.node import VectorNode
+from vectormesh.models import QueryRequest, SearchResult
+
+__all__ = ["MeshEngine", "VectorNode", "QueryRequest", "SearchResult"]
